91d54d8ff460917c9c1a3f1ae9f46b3cf09dd869
ARK7X <giraldino12@gmail.com>
2026-09-18 20:03:25 -0500

    Added decisions to control the empty forms also added visual feedback to let the user know what is happining

--- src/components/InputDescription.tsx
+++ src/components/InputDescription.tsx
@@ -1,18 +1,25 @@
 type InputProps = {
   label: string;
   placeHolder: string;
-  value: string
-  handleOnChangeInput?: (
-    name:string,
-    amount:string
-  ) => void
+  value: string;
+  isEmpty?: boolean;
+  handleOnChangeInput?: (name: string, amount: string) => void;
 };
 
 export const InputDescription = (props: InputProps) => {
   return (
-    <div className="InputDescription">
-        <label htmlFor="">{props.label}</label>
-      <input type="text" placeholder={props.placeHolder} name={props.label} value={props.value} onChange={(e) => props.handleOnChangeInput?.(e.target.name, e.target.value)}/>
+    <div className={"InputDescription "}>
+      <label htmlFor={props.label}>{props.label}</label>
+      <input
+        className={props.isEmpty ? "isEmpty" : "isNotEmpty"}
+        type="text"
+        placeholder={props.placeHolder}
+        name={props.label}
+        value={props.value}
+        onChange={(e) =>
+          props.handleOnChangeInput?.(e.target.name, e.target.value)
+        }
+      />
     </div>
   );
 };
