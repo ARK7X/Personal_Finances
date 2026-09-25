cc178a303e0077e2ad053133f46997f8c4d148c4
ARK7X <giraldino12@gmail.com>
2026-09-25 16:02:34 -0500

    Removed all TS code from component cardControl and moved to cardFunctionalitieshool in order to make more clear the readings of the components also change the way the user see the edit functionality is no longer on it own card anymore the edit is now using the main cardControl with it´s proper messages for a better feedback

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -5,6 +5,7 @@ import { InputDescription } from "./InputDescription";
 import { useCardFunctionalitiesHook } from "../hooks/CardFunctionalities";
 
 export const CardContainer = () => {
+
   const cardFunctionalities = useCardFunctionalitiesHook();
 
   return (
@@ -20,22 +21,15 @@ export const CardContainer = () => {
                 label="Description"
                 placeHolder="Type your description"
                 value={cardFunctionalities.Forms.description}
+                disabled={true}
                 handleOnChangeInput={cardFunctionalities.handleOnChangeInput}
               />
               <InputDescription
                 label="Amount"
                 placeHolder="Type your Amount"
                 value={cardFunctionalities.Forms.amount}
+                disabled={true}
                 handleOnChangeInput={cardFunctionalities.handleOnChangeInput}
-              />
-              <InputButton
-                xmlns="http://www.w3.org/2000/svg"
-                classID="confirmButton"
-                path="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"
-                viewBox="0 0 16 16"
-                handleClick={() =>
-                  cardFunctionalities.handleConfirmEdit(item.id)
-                }
               />
             </div>
           ) : (
@@ -60,7 +54,16 @@ export const CardContainer = () => {
           ),
         )}
         </div>
-        <CardControl handleClick={cardFunctionalities.handleClick} />
+        <CardControl 
+          valueDescription={cardFunctionalities.Forms.description} 
+          valueAmount={cardFunctionalities.Forms.amount}
+          isAmountEmpty={cardFunctionalities.isAmountEmpty}
+          isDescriptionEmpty={cardFunctionalities.isDescriptionEmpty}
+          editClicked={cardFunctionalities.isEditClicked}
+          editId={cardFunctionalities.editId}
+          handleConfirmEdit={cardFunctionalities.handleConfirmEdit}
+          handleOnChangeInput={cardFunctionalities.handleOnChangeInput}
+          handleSendForm={cardFunctionalities.handleSendForm}/>
       </div>
     </>
   );
--- src/components/CardControl.tsx
+++ src/components/CardControl.tsx
@@ -1,43 +1,19 @@
 import { InputDescription } from "./InputDescription";
 import { InputButton } from "./InputButton";
-import { useState } from "react";
 
 type propsCardControl = {
-  handleClick: (id: string, description: string, amount: string) => void;
-};
+  valueDescription:string,
+  valueAmount:string,
+  isDescriptionEmpty:boolean,
+  isAmountEmpty:boolean,
+  editClicked:boolean,
+  editId:string,
+  handleConfirmEdit: (id:string) => void,
+  handleOnChangeInput: (name: string, value: string) => void,
+  handleSendForm: () => void;
+}
 
-type forms = {
-  description: string;
-  amount: string;
-};
-
-export const CardControl = (props: propsCardControl) => {
-  const [forms, setForms] = useState<forms>({ description: "", amount: "" });
-  const [isDescriptionEmpty, setIsDescriptionEmpty] = useState<boolean>(true);
-  const [isAmountEmpty, setIsAmountEmpty] = useState<boolean>(true);
-
-  const handleOnChangeInput = (name: string, value: string) => {
-    const whichInput: boolean = name === "Description";
-    if (whichInput) {
-      setForms({ ...forms, description: value });
-      value.length === 0
-        ? setIsDescriptionEmpty(true)
-        : setIsDescriptionEmpty(false);
-    } else {
-      setForms({ ...forms, amount: value });
-      value.length === 0 ? setIsAmountEmpty(true) : setIsAmountEmpty(false);
-    }
-  };
-
-  const sendForm = () => {
-    if (isDescriptionEmpty !== true && isAmountEmpty !== true) {
-      let idCategory: string = "income" + Math.random();
-      props.handleClick(idCategory, forms.description, forms.amount);
-      setForms({ ...forms, description: "", amount: "" });
-      setIsAmountEmpty(true);
-      setIsDescriptionEmpty(true);
-    }
-  };
+export const CardControl = (props:propsCardControl) => {
 
   return (
     <>
@@ -45,28 +21,45 @@ export const CardControl = (props: propsCardControl) => {
         <InputDescription
           label="Description"
           placeHolder="Description"
-          value={forms.description}
-          isEmpty={isDescriptionEmpty}
-          handleOnChangeInput={handleOnChangeInput}
+          value={props.valueDescription}
+          isEmpty={props.isDescriptionEmpty}
+          handleOnChangeInput={props.handleOnChangeInput}
         />
         <InputDescription
           label="Amount"
           placeHolder="Amount"
-          value={forms.amount}
-          isEmpty={isAmountEmpty}
-          handleOnChangeInput={handleOnChangeInput}
+          value={props.valueAmount}
+          isEmpty={props.isAmountEmpty}
+          handleOnChangeInput={props.handleOnChangeInput}
         />
-        <InputButton
-          xmlns="http://www.w3.org/2000/svg"
-          classID="addButton"
-          path="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0"
-          viewBox="0 0 16 16"
-          handleClick={sendForm}
-        />
-        {(isDescriptionEmpty || isAmountEmpty) ? (
-          <p className="fillWarning">You must fill all the empty spaces</p>
-        ):
-          <p className="fillSuccess">Great!</p>
+        {props.editClicked ?
+          <InputButton
+              xmlns="http://www.w3.org/2000/svg"
+              classID="confirmButton"
+              path="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"
+              viewBox="0 0 16 16"
+              handleClick={() => props.handleConfirmEdit(props.editId)}
+          />
+        :
+          <InputButton
+              xmlns="http://www.w3.org/2000/svg"
+              classID="addButton"
+              path="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0"
+              viewBox="0 0 16 16"
+              handleClick={props.handleSendForm}
+          />
+        }
+        {
+          props.editClicked ?
+            (props.isAmountEmpty !== true || props.isDescriptionEmpty !== true) ?
+            <p className="fillSuccess">Great!</p>
+            :
+            <p className="editWarning">You must at least modify one parameter</p>
+          :
+            (props.isAmountEmpty || props.isDescriptionEmpty) ?
+            <p className="fillWarning">You must fill all the empty spaces</p>
+            :
+            <p className="fillSuccess">Great!</p>
         }
       </div>
     </>
